8a3f1a4543a34b9c8f19f26dbc9d155d4b11bac6
Jakub Marousek <marousej@artax.karlin.mff.cuni.cz>
2016-03-22 11:22:25 +0100

    Dirs for templates and first view

--- ekonomicka/urls.py
+++ ekonomicka/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+import recipes
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^recipes/', include('recipes.urls'))
 ]
