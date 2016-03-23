cf87a058e5c05f9791589f28404bf4c74a69e34c
Jakub Marousek <marousej@artax.karlin.mff.cuni.cz>
2016-03-23 19:02:10 +0100

    Validation for licences

--- core/models.py
+++ core/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 import timedelta
+from django.core.exceptions import ValidationError
 
 # Single row to hold game state
 class Game(models.Model):
@@ -20,8 +21,17 @@ class Team(models.Model):
     name = models.CharField(max_length=256, unique=True)
     visible = models.BooleanField(default=False)
 
+    def balance(self):
+        """
+        Returns team's balance as a dictionary { entity:balance }
+        :rtype Dict[Entity, Balance]
+        """
+        bal = Balance.objects.filter(team=self)
+        return { balance.entity:balance for balance in bal }
+
     def __str__(self):
         return self.name
+
 
 class Player(models.Model):
     user = models.OneToOneField(User)
@@ -56,8 +66,37 @@ class Entity(models.Model):
 class Balance(models.Model):
     team = models.ForeignKey(Team)
     entity = models.ForeignKey(Entity)
-    amount = models.PositiveIntegerField()
-    blocked = models.PositiveIntegerField()
+    amount = models.PositiveIntegerField(default=0)
+    blocked = models.PositiveIntegerField(default=0)
+
+    def total(self):
+        return self.amount + self.blocked
+
+    def clean(self):
+        """
+        Checks whether the team owns a licence for the item;
+        When the item itself is a licence, check the count of dependent items is zero
+
+        NOTE: When transferring a group of items, make sure licences are saved first -- otherwise, the following
+        constraint fails
+        """
+        licence = self.entity.licence
+        team_balance = self.team.balance()
+        have_licence = False
+        try:
+            if licence is None or team_balance[licence].total() > 0:
+                have_licence = True
+        except KeyError:
+            pass
+        if self.total() > 0 and not have_licence:
+            raise ValidationError("Cannot own %s, licence not present" % self.entity)
+
+        if self.amount==0: # blocked licences don't count
+            for entity, balance in team_balance.items():
+                if entity.licence==self.entity and balance.amount > 0:
+                    raise ValidationError("Cannot set %s amount to zero, %s depends on it" % (self, entity))
+
+
 
     def __str__(self):
         return "%s: %s (%d + %d blocked) " % (self.team.name, self.entity.name, self.amount, self.blocked)
