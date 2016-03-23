f594f529d74feac22e871dfe0bf5c07172715e29
Jakub Marousek <marousej@artax.karlin.mff.cuni.cz>
2016-03-23 14:36:07 +0100

    Team balance in administration

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from .models import *
 
+class BalancesInline(admin.TabularInline):
+    model = Balance
+
+class TeamAdmin(admin.ModelAdmin):
+    inlines = [ BalancesInline ]
+
 admin.site.register(Player)
-admin.site.register(Team)
+admin.site.register(Team, TeamAdmin)
 
 admin.site.register(Entity)
 
