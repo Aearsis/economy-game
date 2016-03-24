45dce36db287505fda82aec19d37a59ede7debb5
Jakub Marousek <marousej@artax.karlin.mff.cuni.cz>
2016-03-24 18:16:41 +0100

    Main function for transferring entities and user-level checking

--- core/models.py
+++ core/models.py
@@ -1,6 +1,7 @@
-from django.db import models
+from django.db import models, transaction
 from django.contrib.auth.models import User
 from django.utils import timezone
+from copy import deepcopy
 import timedelta
 from django.core.exceptions import ValidationError
 
@@ -22,6 +23,9 @@ class Team(models.Model):
     visible = models.BooleanField(default=False)
     balance = None
 
+
+    ### Team balance ###
+
     def get_balance(self, entity):
         """
         Returns balance of a entity or creates new if not yet defined
@@ -34,11 +38,109 @@ class Team(models.Model):
         return balance[entity]
 
     def get_balance_complete(self):
+        """
+        Returns team's complete balance
+        :rtype dict(Entity, Balance)
+        """
         if not self.balance:
             bal_set = Balance.objects.filter(team=self)
             self.balance = { balance.entity:balance for balance in bal_set }
 
         return self.balance
+
+
+    ### Trading and transferring entities
+
+    @transaction.atomic
+    def transfer(self, pretend, entities_amounts_in=[], entities_amounts_out=[], \
+                 in_blocked=False, out_blocked=False):
+        """
+            Transfers entities between teams or system.
+
+            :param pretend: If set to True, checks whether the transaction is possible and returns bool.
+                            If set to False, perform the transaction and raise ValidationError.
+            :param entities_amounts_in: List of EntityAmounts which the team gives away
+            :param entities_amounts_out: List of EntityAmounts that the team accepts
+            :param in_blocked: Incoming entites are blocked
+            :param out_blocked: Outgoing entities are blocked
+        """
+
+        tmp_balance = deepcopy(self.get_balance_complete()) # we do all work on a copy of team's balance
+        licence_map = Entity.map_licences(tmp_balance.keys()) # inverse licence mapping
+
+        def get_tmp_balance(entity):
+            if entity not in tmp_balance:
+                tmp_balance[entity] = Balance(team=self, entity=entity, amount=0, blocked=0)
+            return tmp_balance[entity]
+
+        # add/subtracts amounts and check for non-negativity
+        def entity_count(ent_am, add):
+            balance = get_tmp_balance(ent_am.entity)
+            mul = 1 if add else -1
+            if (add and in_blocked) or (not add and out_blocked):
+                balance.blocked += mul*ent_am.amount
+                if balance.blocked < 0:
+                    raise ValidationError(str(ent_am.entity)+" is negative")
+            else:
+                balance.amount += mul*ent_am.amount
+                print(ent_am.entity,balance.amount)
+                if balance.amount < 0:
+                    raise ValidationError(str(ent_am.entity)+" is negative")
+
+        def licence_check(ent_am):
+            this_amount = get_tmp_balance(ent_am.entity).amount
+            if ent_am.entity.licence: # this entity depends on a licence
+                licence_amount = get_tmp_balance(ent_am.entity.licence).amount
+                if licence_amount==0 and this_amount > 0:
+                    raise ValidationError("No licence for "+str(ent_am.entity))
+
+            if this_amount==0 and ent_am.entity in licence_map: # this entity is a licence and we have none
+                for dep in licence_map[ent_am.entity]:
+                    if get_tmp_balance(dep).amount > 0:
+                        raise ValidationError("%s depends on %s" % (dep, ent_am.entity))
+
+        def commit(ent_am):
+            if not pretend:
+                get_tmp_balance(ent_am.entity).save()
+
+        # To save items to DB in the correct order, we need to distinguish between licence and dependent items.
+        # Here call "licences" all entities that have its licence entry None.
+        # Order of checking/saving:
+        #  outgoing non-licences
+        #  outgoing licences
+        #  incoming licences
+        #  incoming non-licences
+
+        out_sorted = sorted(entities_amounts_out, key=lambda ent_am: int(ent_am.entity.licence is None))
+        in_sorted = sorted(entities_amounts_in, key=lambda ent_am: int(ent_am.entity.licence is not None))
+
+        try:
+            for ent_am in out_sorted:
+                entity_count(ent_am, add=False)
+                licence_check(ent_am)
+                commit(ent_am)
+
+            for ent_am in in_sorted:
+                entity_count(ent_am, add=True)
+                licence_check(ent_am)
+                commit(ent_am)
+
+            # save the cache back to the object
+            if not pretend:
+                self.balance = tmp_balance
+            return True
+        except ValidationError:
+            if pretend:
+                return False
+            else:
+                raise
+
+
+    def can_block(self, ent_am):
+        pass
+
+    def can_give(self, ent_am):
+        pass
 
     def __str__(self):
         return self.name
@@ -63,10 +165,26 @@ class Status(models.Model):
     class Meta:
         verbose_name_plural = "Statuses"
 
+
 class Entity(models.Model):
     name = models.CharField(max_length=128)
     units = models.CharField(max_length=128,blank=True)
     licence = models.ForeignKey("self", null=True, blank=True)
+
+    @staticmethod
+    def map_licences(entities):
+        """
+        Accepts a list of entities and returns a dictionary { entity E, list(entity depending on E }
+        ( inverses licence mapping)
+        """
+        map = {}
+        for entity in entities:
+            if entity.licence:
+                if entity.licence not in map:
+                    map[entity.licence] = []
+                map[entity.licence].append(entity)
+
+        return map
 
     def __str__(self):
         return self.name
@@ -106,11 +224,25 @@ class Balance(models.Model):
                 if entity.licence==self.entity and balance.amount > 0:
                     raise ValidationError("Cannot set %s amount to zero, %s depends on it" % (self, entity))
 
-
-
     def __str__(self):
         return "%s: %s (%d + %d blocked) " % (self.team.name, self.entity.name, self.amount, self.blocked)
 
     class Meta:
         unique_together = ("team", "entity")
+
+
+class EntityAmount:
+    """
+    Helper class for pairs of entity and amount
+    """
+    entity = Entity()
+    amount = 0
+
+    def __repr__(self):
+        return "%s (%s %s)" % (self.entity, self.amount, self.entity.units)
+
+    def __init__(self, entity, amount):
+        self.entity = entity
+        self.amount = amount
+
 
