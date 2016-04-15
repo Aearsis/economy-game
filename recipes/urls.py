ce3c99845006c3edf56828bfd6fa041e6ea83ad3
Jakub Marousek <marousej@artax.karlin.mff.cuni.cz>
2016-04-16 00:12:23 +0200

    Menu item, creating auction JS bug

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 app_name = "recipes"
 urlpatterns = [
-    url(r'^$', 'recipes.views.index'),
+    url(r'^$', 'recipes.views.index', name='index'),
     url(r'^detail/([0-9]+)', 'recipes.views.detail', name='detail')
 ]
