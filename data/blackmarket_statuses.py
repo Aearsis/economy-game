cd6c53c914c1ea68020bd6ab2730df21cab33918
Kateřina Zákravská <zakravska.katerina@gmail.com>
2016-04-18 00:13:28 +0200

    Hlášky od Katky a Péti

--- data/blackmarket_statuses.py
+++ data/blackmarket_statuses.py
@@ -7,10 +7,43 @@ def fair_status():
     #  %(seller)s - jméno prodávajícího (Franta Vomáčka)
     #  %(buyer)s - kupující tým (Kamenolom)
     #  %(bought)s - koupeno (1ks Izolepa, 1ks Kámen a 3m Dobrej pocit)
-    #  %(sold)d - prodáno (1ks Izolepa, 1ks Kámen a 3m Dobrej pocit)
+    #  %(sold)s - prodáno (1ks Izolepa, 1ks Kámen a 3m Dobrej pocit)
     return choice([
         "Tým %(buyer)s koupil od %(seller)s %(bought)s za %(sold)s.",
         "Tým %(seller)s málem nedodal %(bought)s pro %(buyer)s, ale nakonec vše dorazilo v pořádku.",
+        "%(seller)s právě výhodně prodal %(sold)s."
+        "%(buyer)s právě výhodně koupil %(bought)s."
+        "Úspěšně proběhla výměna %(bought)s a %(sold)s mezi %(buyer)s a %(seller)s."
+        "Tým %(buyer)s právě nakoupil %(bought)s. No... kdo chce kam, pomožme mu tam."
+        "Za zvýhodněnou cenu %(sold)s právě %(buyer)s nakoupil %(bought)s od %(seller)s."
+        "Tým %(buyer)s právě nakoupil %(bought)s. Otázkou je, k čemu jim to bude?"
+        "Tým %(buyer)s právě nakoupil %(bought)si. Stojí za úvahu, zda to bylo dobře."
+        "Právě bylo prodáno %(sold)s za %(bought)s."
+        "%(buyer)s zakoupil %(bought)s za %(sold)s."
+        "Už je prodáno %(sold)s za cenu %(bought)s."
+        "Poprvé, podruhé, potřetí. Prodáno %(sold)s týmu %(buyer)s za %(bought)s."
+        "Týmu %(buyer)s bylo přiklepnuto %(bought)s za %(sold)s."
+        "Hroší bozi požehnali a urodilo se %(bought)s, což bylo v zápětí prodáno týmu %(buyer)s."
+        "Nejvyšší hroch svolil k výměně mezi %(seller)s a %(buyer)s."
+        "Piraní dopravní služba právě dovezla %(bought)s k %(buyer)s."
+        "Mamutí nákladní expres právě fofrem dovezl %(bought)s k %(buyer)s."
+        "Týmu %(buyer)s šavlozubí holubi doručili zásilku %(bought)s."
+        "Zásilka %(sold)s byla přepadena kamenožrouty a zničena. %(buyer)s obdžel %(bought)s, avšak odmítl nahradit škodu."
+        "Byl uzavřen obchod mezi %(seller)s a %(buyer)s."
+        "%(seller)s prodal %(sold)s."
+        "%(buyer)s koupil %(bought)s."
+        "Za %(bought)s bylo zakoupeno %(sold)s."
+        "Tým %(buyer)s si objednal %(bought)s."
+        "Tým %(buyer)s zaplatil vysokou cenu %(sold)s, vyšlo ho to fakt draze."
+        "Právě proběhl pokus o krádež %(bought)s, ale %(buyer)s byl načapán a musel zaplatit %(sold)s."
+        "Přesně %(sold)s stálo %(buyer)s koupit %(bought)s."
+        "Tak tak se stihl %(seller)s zbavit %(sold)s před datem expirace."
+        "%(buyer)s právě prohloupil nákupem %(bought)s, už ale svoje %(sold)s nikdy neuvidí."
+        "%(buyer)s výhodně získal %(bought)s."
+        "Vyměnili byste %(sold)s za %(bought)s? Tým %(buyer)s to právě udělal."
+        "%(seller)s ví, jak se rychle zbavit %(sold)s."
+        "%(buyer)s vyhodil právě %(sold)s z okna, literárně řečeno."
+        "%(seller)s se zrovna zbavil %(sold)s."
         # ...
     ])
 
@@ -21,6 +54,7 @@ def fake_status():
 
     return choice([
         "%(seller)s uzavrel obchod s týmem %(buyer)s o koupi %(advertised)s, ale příšlo místo toho %(bought)s!",
+        "Být či nebýt... to není správná otázka. Koupit či nekoupit, to je oč tu běží."
         # ...
     ])
 
